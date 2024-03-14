6d6831f5c7fccd05aa004fcebabf03c9026c5f28
kanon-ify <ributkukuh270@gmail.com>
2024-03-14 10:45:48 +0700

    gpu: add freq 780mhz
    Added freq 780mhz but remove 596mhz because maximum just 9 frequency step

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -281,9 +281,9 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(342000000, P_CRC_DIV,  1, 0, 0),
 	F(414000000, P_CRC_DIV,  1, 0, 0),
 	F(520000000, P_CRC_DIV,  1, 0, 0),
-	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
+	F(780000000, P_CRC_DIV,  1, 0, 0),
 	F(820000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
@@ -322,9 +322,9 @@ static struct clk_rcg2 gpu_cc_gx_gfx3d_clk_src = {
 			LOW, 280000000,
 			LOW_L1, 338000000,
 			NOMINAL, 425000000,
-			NOMINAL_L1, 487000000,
-			HIGH, 548000000,
-			HIGH_1, 600000000, 
+			NOMINAL_L1, 548000000,
+			HIGH, 600000000,
+			HIGH_1, 650000000, 
 			HIGH_L1, 700000000),
 	},
 };
@@ -639,10 +639,10 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 		414000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL] = 520000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
-		596000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 675000000;
+		675000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 710000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_1] =
-		710000000;
+		780000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
 		820000000;
 }
